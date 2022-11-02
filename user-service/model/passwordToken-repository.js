582d6e27f4228c9047d78f167ff0fea02e282f16
tandeshao <e0544387@u.nus.edu>
2022-11-03 01:19:16 +0800

    update mongo_db_uri env var

--- user-service/model/passwordToken-repository.js
+++ user-service/model/passwordToken-repository.js
@@ -7,9 +7,7 @@ import mongoose from 'mongoose';
 // let mongoDB = process.env.ENV == "PROD" ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
 const PASSWORD = process.env.PASSWORD
 const DB_NAME = process.env.DB_NAME
-let ATLAS_URI = process.env.ENV == "DEV" ? process.env.DB_LOCAL_URI :
-    `mongodb+srv://username:${PASSWORD}@peerprep-cluster.wcw5ljh.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
-    || process.env.DB_LOCAL_URI;
+let ATLAS_URI = process.env.ENV == "DEV" ? process.env.DB_LOCAL_URI : process.env.MONGODB_URI
 
 mongoose.connect(ATLAS_URI,
     { useNewUrlParser: true,
